834bb919e7ae816ef203c1648f03981a586a3d7b
Daviiap <davii.sapiens@gmail.com>
2024-07-07 20:24:43 -0300

    removing debug function

--- main.go
+++ main.go
@@ -3,27 +3,11 @@ package main
 import (
 	"go_rest_api/database"
 	"go_rest_api/handlers"
-	"go_rest_api/models"
 	"go_rest_api/repositories"
 	"go_rest_api/services"
-	"go_rest_api/utils"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
-
-func createUser(db *gorm.DB) {
-	hashPasswd, _ := utils.HashPassword("123")
-
-	user := &models.User{
-		ID:       "1",
-		Name:     "jon doe",
-		Email:    "jondoe@email.com",
-		Password: hashPasswd,
-	}
-
-	db.Create(user)
-}
 
 func main() {
 	db, err := database.GetDatabaseConnection()
@@ -34,8 +18,6 @@ func main() {
 	userRepo := repositories.NewUserRepo(db)
 	loginService := services.NewLoginService(userRepo)
 	loginHandler := handlers.NewLoginHandler(loginService)
-
-	createUser(db)
 
 	router := gin.New()
 	v1 := router.Group("/v1")
